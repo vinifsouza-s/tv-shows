b387f88c403b335f87a06fdd6ca3bbe92b885e65
vinifsouza-s <vinicius.sfs91@gmail.com>
2024-09-04 10:38:36 -0300

    feature: folder structure to follow Atomic Design principles, also created episodes and series cards

---
+++ src/atomic/molecules/SeriesCards/index.tsx
@@ -0,0 +1,20 @@
+// src/components/SeriesItem.tsx
+
+import React from 'react';
+import './styles.scss';
+import { SeriesProps } from '../../../types/Series';
+
+
+interface SeriesCardsProps {
+    series: Pick<SeriesProps, 'id' | 'name' | 'image'>;
+    onClick: () => void;
+}
+
+const SeriesCards: React.FC<SeriesCardsProps> = ({ series, onClick }) => (
+    <div className="series-item" onClick={onClick}>
+        <img src={series.image.medium} alt={series.name} />
+        <h2>{series.name}</h2>
+    </div>
+);
+
+export default SeriesCards;
